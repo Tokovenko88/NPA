f3fc5c6bb47271b7e6f8b753155df567491da08e
Tokovenko88 <tokovenkoanatolij@gmail.com>
2026-08-19 18:48:09 +0300

    refactor: expose canonical domain helpers

--- npa_processor/domain/__init__.py
+++ npa_processor/domain/__init__.py
@@ -1,1 +1,12 @@
 """Domain-level value objects and canonical vocabularies for NPA processing."""
+
+from npa_processor.domain.dates import format_npa_date, format_npa_date_iso, parse_npa_date
+from npa_processor.domain.element_types import TYPE_TO_RUSSIAN, normalize_ru_type
+
+__all__ = [
+    "TYPE_TO_RUSSIAN",
+    "format_npa_date",
+    "format_npa_date_iso",
+    "normalize_ru_type",
+    "parse_npa_date",
+]
