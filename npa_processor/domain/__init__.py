256ab77c3d56ddaeeb1318604e52ad77c1369ff6
Tokovenko88 <tokovenkoanatolij@gmail.com>
2026-08-19 18:55:52 +0300

    Expose domain preflight API

--- npa_processor/domain/__init__.py
+++ npa_processor/domain/__init__.py
@@ -6,11 +6,13 @@ from npa_processor.domain.dates import (
     parse_npa_date,
 )
 from npa_processor.domain.element_types import TYPE_TO_RUSSIAN, normalize_ru_type
+from npa_processor.domain.preflight import PreflightIssue, run_preflight, validate_document, validate_source_dates
 from npa_processor.domain.rebuild_plan import ancestor_ids, build_parent_map, rebuild_order
 from npa_processor.domain.reference_integrity import find_reference_issues
 
 __all__ = [
     "TYPE_TO_RUSSIAN",
+    "PreflightIssue",
     "ancestor_ids",
     "build_parent_map",
     "find_reference_issues",
@@ -19,4 +21,7 @@ __all__ = [
     "normalize_ru_type",
     "parse_npa_date",
     "rebuild_order",
+    "run_preflight",
+    "validate_document",
+    "validate_source_dates",
 ]
